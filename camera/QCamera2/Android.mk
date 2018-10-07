09170a279057a99061859b0d3eb56493736de9bb
aashishdamrait <aashishas@outlook.com>
2018-10-07 14:01:08 +0530

    Fix Derp

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -113,7 +113,7 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display-caf/msm8996/libqservice 
+        $(call project-path-for,qcom-display)/libqservice 
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
